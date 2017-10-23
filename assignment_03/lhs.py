c40e9cd2a9262a87a43da61016fea3b9f4d6b2bb
Niekmereu <mereu.niek@gmail.com>
2017-10-23 12:31:39 +0200

    frpkfpk

--- assignment_03/lhs.py
+++ assignment_03/lhs.py
@@ -10,23 +10,28 @@ import pandas as pd
 from scipy.spatial.distance import pdist, jaccard
 from scipy.spatial.distance import squareform
 from collections import defaultdict
+import pickle
 
-a = np.repeat(list(range(20)), 5)
+a = np.repeat(list(range(25)), 6)
 a = np.array(random.sample(list(a), len(a)))
 
+S = pickle.load( open( "sig_matrix.p", "rb" ) )
+print(S.shape)
+S = S[:,0:1000]
+
 n = 100
-Dataset = (np.random.randint(1, 8, 2000)).reshape(n, 20)
-B = 20
+#Dataset = (np.random.randint(1, 8, 2000)).reshape(n, 20)
+B = 25
 R = n/B
-Test = pd.DataFrame(Dataset[np.where(a == 1)[0]])
+Test = pd.DataFrame(S[np.where(a == 1)[0]])
 v = Test.values              
 #lt = pd.DataFrame((v[:, None] == v.T))
 #np.fill_diagonal(lt.values, 0)
 res = 1 - pdist(Test.T, 'jaccard')
 squareform(res)
-distance = np.triu(pd.DataFrame(squareform(res), index = Test.T.index, 
-                        columns = Test.T.index), k = 0)
-distance = np.round(distance, decimals = 1)
+distance = round(np.triu(pd.DataFrame(squareform(res), index = Test.T.index, 
+                        columns = Test.T.index), k = 0), ndigits = 1)
+
 
 
 
@@ -34,6 +39,5 @@ d = defaultdict(list)
 for pos, val in np.ndenumerate(distance):
     if val:
         d[val].append((pos[0], pos[1]))
-        
 
-print(d)
+
